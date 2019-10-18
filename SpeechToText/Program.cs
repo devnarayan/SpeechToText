541fb04233a90e3e8a61b0d378496815df43118c
devnagar <dev.nagar@dadenllc.com>
2019-10-18 12:42:15 +0530

    Add project files.

---
+++ SpeechToText/Program.cs
@@ -0,0 +1,77 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
+using Grpc.Auth;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EncodeSignleChannel();
+
+               GoogleCredential googleCredential;
+            using (Stream m = new FileStream("SpeechToText-e03321616d8f.json", FileMode.Open))
+                googleCredential = GoogleCredential.FromStream(m);
+            var channel = new Grpc.Core.Channel(SpeechClient.DefaultEndpoint.Host,
+                googleCredential.ToChannelCredentials());
+            var speech = SpeechClient.Create(channel);
+
+            var config = new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                //SampleRateHertz = 44100,
+                //AudioChannelCount=1,
+                LanguageCode = LanguageCodes.English.UnitedStates
+            };
+
+            var audio = RecognitionAudio.FromFile("channel1.wav");
+            var response = speech.Recognize(config, audio);
+            foreach(var result in response.Results)
+            {
+                foreach(var alternative in result.Alternatives)
+                {
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
+        }
+
+       static void EncodeSignleChannel()
+        {
+            var reader = new WaveFileReader("audit.wav");
+            var buffer = new byte[2 * reader.WaveFormat.SampleRate * reader.WaveFormat.Channels];
+            var writers = new WaveFileWriter[reader.WaveFormat.Channels];
+            for (int n = 0; n < writers.Length; n++)
+            {
+                var format = new WaveFormat(reader.WaveFormat.SampleRate, 16, 1);
+                writers[n] = new WaveFileWriter(String.Format("channel{0}.wav", n + 1), format);
+            }
+            int bytesRead;
+            while ((bytesRead = reader.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                int offset = 0;
+                while (offset < bytesRead)
+                {
+                    for (int n = 0; n < writers.Length; n++)
+                    {
+                        // write one sample
+                        writers[n].Write(buffer, offset, 2);
+                        offset += 2;
+                    }
+                }
+            }
+            for (int n = 0; n < writers.Length; n++)
+            {
+                writers[n].Dispose();
+            }
+            reader.Dispose();
+        }
+    }
+}
---
+++ SpeechToText/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SpeechToText")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpeechToText")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05f4d471-9369-4429-86d9-b9bbc3143b64")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
